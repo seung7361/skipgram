82b894ca0c2f86e5e71dd1ba2f99192a72237151
seung7361 <seung7361@naver.com>
2023-06-29 10:37:10 +0900

    preprocessing done

---
+++ preprocessing.py
@@ -0,0 +1,79 @@
+import json
+from tqdm import tqdm
+from datasets import load_dataset
+
+class Tokenizer:
+    def __init__(self):
+        self.vocab = {}
+        self.reverse_vocab = {}
+        self.vocab_size = 0
+
+        self.special_tokens = [
+            '[UNK]',
+            '[SOS]',
+            '[EOS]',
+            '[PAD]'
+        ]
+    
+    def add_word(self, word):
+        if word not in self.vocab:
+            self.vocab[word] = self.vocab_size
+            self.reverse_vocab[self.vocab_size] = word
+            self.vocab_size += 1
+    
+    def load(self):
+        with open('tokenizer.json', 'r') as f:
+            file = json.load(f)
+        
+        if len(file['vocab']) == 0:
+            print('Tokenizer is empty')
+
+            wikitext = load_dataset('wikitext', 'wikitext-103-v1')['train']['text']
+
+            for sentence in tqdm(wikitext):
+                for word in sentence.split():
+                    word = word.lower()
+                    if word not in self.vocab:
+                        self.add_word(word)
+
+            print('wikitext dataset preprocessing done successfully')
+
+            file['vocab'] = self.vocab
+            file['reverse_vocab'] = self.reverse_vocab
+
+            # save file
+            with open('tokenizer.json', 'w') as f:
+                json.dump(file, f)
+        
+        else:
+            print('Loading Tokenizer from tokenizer.json...')
+
+            self.vocab = file['vocab']
+            self.reverse_vocab = file['reverse_vocab']
+            self.vocab_size = len(self.vocab)
+
+            print('load complete')
+        
+        # process special tokens
+
+        for token in self.special_tokens:
+            self.add_word(token)
+    
+    def word_to_idx(self, word):
+        if word in self.vocab:
+            return self.vocab[word]
+        else:
+            return self.vocab['[UNK]']
+    
+    def idx_to_word(self, index):
+        index = str(index)
+        if index in self.reverse_vocab:
+            return self.reverse_vocab[index]
+        else:
+            return -1
+
+    def tokenize(self, sentence):
+
+        return [self.word_to_idx(word) for word in sentence.split()]
+    
+    
--- skipgram.py
+++ skipgram.py
@@ -1,10 +1,17 @@
 import numpy as np
 import torch
 
+from preprocessing import Tokenizer
+
+tokenizer = Tokenizer()
+tokenizer.load()
+
 class EmbeddingLayer(torch.nn.Module):
     def __init__(self, num_embeddings: int, embedding_dim: int):
         super().__init__()
 
+        # num_embeddings == vocab_size
+        # embedding_dim == depth
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
 
@@ -16,3 +23,71 @@ class EmbeddingLayer(torch.nn.Module):
         embeddings = self.weights[inputs]
 
         return embeddings
+
+class LinearLayer(torch.nn.Module):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.weights = torch.nn.Parameter(torch.randn(input_dim, output_dim))
+        self.bias = torch.nn.Parameter(torch.randn(output_dim))
+    
+    def forward(self, inputs):
+        outputs = torch.matmul(inputs, self.weights) + self.bias
+
+        return outputs
+
+class Skipgram(torch.nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int):
+        super().__init__()
+
+        self.embedding_layer = EmbeddingLayer(num_embeddings, embedding_dim)
+        self.linear_layer = LinearLayer(embedding_dim, num_embeddings)
+    
+    def forward(self, inputs):
+        # inputs: (batch_size, seq_len)
+
+        embeddings = self.embedding_layer(inputs)
+        # embeddings: (batch_size, seq_len, embedding_dim)
+
+        out = self.linear_layer(embeddings)
+        # out: (batch_size, seq_len, num_embeddings) => logit values
+        
+        out = torch.nn.functional.softmax(out, dim=1)
+        # out: (batch_size, seq_len, num_embeddings) => probability values
+
+        return out
+
+def train_skipgram(model, num_epochs: int, data, lr: float):
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
+    loss_fn = torch.nn.NLLLoss()
+
+    for epoch in range(num_epochs):
+        total_loss, cnt = 0.0, 0
+
+        for context in data:
+            input = context.split()
+            if len(input) < 10:
+                continue
+            
+            context = torch.LongTensor([tokenizer.word_to_idx(word) for word in input])
+            target = context
+
+            optimizer.zero_grad()
+
+            prob = model(context, target)
+
+            loss = loss_fn(prob, target)
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            cnt += 1
+
+        print(f"Epoch: {epoch + 1}/{num_epochs}, Loss: {total_loss / cnt}")
+
+model = Skipgram(tokenizer.vocab_size, 2048)
+print("{:_}".format(sum(p.numel() for p in model.parameters())))
